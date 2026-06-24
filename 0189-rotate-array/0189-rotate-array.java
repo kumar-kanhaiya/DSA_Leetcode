a477cc423678dce7e041a505fe8b1b6285357dc1
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2026-06-24 21:23:48 +0530

    Time: 0 ms (100%), Space: 60.7 MB (98.44%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,19 +1,23 @@
 class Solution {
     public void rotate(int[] arr, int k) {
-        int[] ans = new int[arr.length ];
+        k = k % arr.length ;
         int len = arr.length ;
-        int index = 0;
-        int pivotIndex = len - k % arr.length ;
-        // now write the right side 
-        for(int i = pivotIndex ; i< arr.length ; i++ ){
-            ans[index++] = arr[i];
-        }
-        for(int i = 0 ; i< pivotIndex ; i++){
-            ans[index++] = arr[i];
-        }
-        // now copy the ans into arr 
-        for(int i = 0 ; i < arr.length ; i++){
-            arr[i] = ans[i];
+        int pivot = len - k ;
+        // swap left side 
+        helper(arr , 0 , pivot -1 );
+        // swap right side 
+        helper(arr , pivot , len-1);
+        // swap complete array 
+        helper(arr , 0 , len-1);
+
+    }
+    public static void helper(int[] arr , int start , int end){
+        while(start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
         }
     }
 }
