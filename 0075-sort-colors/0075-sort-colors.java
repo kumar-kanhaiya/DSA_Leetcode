ca8e6db201bb0095f255c77b36a31ddcbc282ecb
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2026-06-26 09:25:40 +0530

    Time: 0 ms (100%), Space: 43 MB (97.14%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,13 +1,25 @@
 class Solution {
     public void sortColors(int[] arr) {
-        for(int i = 0 ; i < arr.length ; i++){
-            for(int j = i+1 ; j< arr.length ; j++){
-                if(arr[i] > arr[j]){
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
+       //optimized approach 
+       for(int i = 0 ; i < arr.length ; i++){
+        int last = arr.length - i - 1;
+        int max = max(arr , 0 , last);
+        swap(arr , max, last);
+
+       }
+    }
+    public static int max(int[] arr , int start , int end ){
+        int max = 0;
+        for(int i = start +1 ; i<= end ; i++){
+            if(arr[i] > arr[max]){
+                max = i;
             }
         }
+        return max;
+    }
+    public static void swap(int[] arr , int first , int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
     }
 }
