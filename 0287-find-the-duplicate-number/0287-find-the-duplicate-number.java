8a113605fa6a13ffc10ac0e718c59b2f71384051
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2026-09-02 14:22:49 +0530

    Time: 6 ms (47.71%), Space: 82.6 MB (81.75%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int findDuplicate(int[] arr) {
+        int i = 0;
+        while(i< arr.length){
+            int correct = arr[i] -1;
+            if(arr[i] != arr[correct]){
+                swap(arr, i , correct);
+            }
+            else{
+                i++;
+            }
+        }
+        // answer return 
+        for(int index = 0 ; index < arr.length ; index++){
+            if(arr[index] != index+1){
+                return arr[index];
+            }
+        }
+        return -1;
+    }
+    public static void swap(int[] arr  , int first , int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
